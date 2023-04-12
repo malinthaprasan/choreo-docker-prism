6fb64d67b094427b96037f5bf573bedbaeacf249
Malintha Amarasinghe <malintha.prasan@gmail.com>
2023-04-12 18:57:29 +0530

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NTE0OQ/spectral.js?branch_slug=master&token=199dcf22-12c3-4f7d-a493-762a9a62e43e";
+export default { extends: ruleset };
